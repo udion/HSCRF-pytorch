97db21d6a0d7e6418f56dca8c3c0b59c575f554a
udion <uddeshya.upa@gmail.com>
2018-05-25 08:02:40 +0000

    example with cuda working

---
+++ model/example2_with_cuda.py
@@ -0,0 +1,28 @@
+import torch
+from torch.autograd import Variable
+from caps_utils import _caps_pri2sec_H
+from caps import Caps1d_primary, Caps1d_secondary
+
+w_s_len = 10
+batch_size = 4
+
+p_in_ch = 1
+n_p_caps = 4
+p_caps_dim = 16
+H = 16
+
+P1 = Caps1d_primary(p_in_ch, n_p_caps, p_caps_dim, cuda_enabled=True)
+sec_in = _caps_pri2sec_H(H, 3, 1, 1, 4)
+
+n_sec_caps=2
+sec_caps_dim=16
+
+S1 = Caps1d_secondary(sec_in, p_caps_dim, n_sec_caps, sec_caps_dim, cuda_enabled=True)
+
+
+x = Variable(torch.rand(w_s_len*batch_size, 1, 16).cuda())
+
+x1 = P1(x)
+x1 = S1(x1)
+
+print(x.size())
